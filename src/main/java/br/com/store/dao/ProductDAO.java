a04e146422d6cc075a3f3c625dcac4654a337273
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-01-15 15:22:29 -0300

    get all method

--- src/main/java/br/com/store/dao/ProductDAO.java
+++ src/main/java/br/com/store/dao/ProductDAO.java
@@ -3,6 +3,7 @@ package br.com.store.dao;
 import br.com.store.model.Product;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ProductDAO {
 
@@ -23,5 +24,14 @@ public class ProductDAO {
     public void remove(Product product) {
         product = this.em.merge(product);
         this.em.remove(product);
+    }
+
+    public Product getById(Long id) {
+        return this.em.find(Product.class, id);
+    }
+
+    public List<Product> getAll() {
+        String jpql = "SELECT p FROM Product p";
+        return this.em.createQuery(jpql, Product.class).getResultList();
     }
 }
--- src/main/java/br/com/store/test/ProductRegister.java
+++ src/main/java/br/com/store/test/ProductRegister.java
@@ -8,9 +8,24 @@ import br.com.store.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class ProductRegister {
     public static void main (String[] args) {
+//        createPorduct();
+        Long id = 4l;
+
+        EntityManager em = JPAUtil.getEntityManager();
+
+        ProductDAO productDAO = new ProductDAO(em);
+
+        List<Product> all = productDAO.getAll();
+        all.forEach(p -> System.out.println(p.getName()));
+
+
+    }
+
+    public static void createPorduct() {
         // creating a product in database
         Category cellphones = new Category("Cellphones");
         Product cellphone = new Product("Samsung S23", "Celular Samsung 23 top de linha da marca", new BigDecimal("5000"), cellphones);
